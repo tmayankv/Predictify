d3d16ce46369cb8ff5c0bd067f08d48ddd5a5088
tmayankv <106771483+tmayankv@users.noreply.github.com>
2023-12-17 15:39:15 +0530

    Sync of Login

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
             </div>
-            <form className="mt-8 space-y-6">
+            <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
               <div className="rounded-md shadow-sm -space-y-px">
                 <div>
                   <label htmlFor="username" className="sr-only">
