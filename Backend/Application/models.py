4c5a7dda3393de7f19a24d4e594920202ee005cf
Yuvraj Bagga <113333290+yuvrajbagga@users.noreply.github.com>
2024-04-18 15:12:51 +0530

    Update models.py

--- Backend/Application/models.py
+++ Backend/Application/models.py
@@ -78,7 +78,7 @@ class Income (db.Model):
 class contactform (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     message = db.Column(db.String(100), nullable=False)
     screenshot = db.Column(db.String(100), nullable=True)
 
