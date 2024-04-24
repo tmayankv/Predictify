fed24feda95bbfeb3c669faa3b7e6f71b1d7c393
Vishesh Phutela <visheshphutela30@gmail.com>
2024-04-24 14:46:52 +0530

    added api for graphs

--- Backend/Application/Api/income.py
+++ Backend/Application/Api/income.py
@@ -5,6 +5,7 @@ from flask_restful import marshal_with
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 import hashlib
+import json
 
 
 from Application.models import *
@@ -16,11 +17,17 @@ from app import app, api, db
 
 class IncomeAPI(Resource):
     @marshal_with(income_fields)
-    def get(self, id=None):
+    def get(self, id=None, username=None):
         if id:
             income = Income.query.get(id)
             if income:
                 return income.to_dict()
+            else:
+                raise NotFoundError(404, 'Income not found')
+        elif username:
+            incomes = Income.query.filter_by(username=username).all()
+            if incomes:
+                return [income.to_dict() for income in incomes]
             else:
                 raise NotFoundError(404, 'Income not found')
         else:
@@ -59,9 +66,33 @@ class IncomeAPI(Resource):
         db.session.commit()
         return {'message': 'Income added successfully'}, 201
     
-        
 
+
+
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+
+
+class GraphAPI(Resource):
+    def get(self, username):
+        incomes = Income.query.filter_by(username=username).all()
+        final = []
+        for income in incomes:
+            inc_dict = {}
+            inc_dict["x"] = Date(income.day, income.month, income.year).__dict__
+            inc_dict["y"] = income.amount
+            final.append(inc_dict)
+
+
+        return jsonify(final)
+            
+    
 
 
 
 api.add_resource(IncomeAPI, '/api/income', '/api/income/<int:id>')
+api.add_resource(GraphAPI, '/api/graph/<string:username>')
