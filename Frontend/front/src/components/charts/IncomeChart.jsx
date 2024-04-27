7f935696b5e8eb9f3ab2980e75f370ec2561e932
tmayankv <106771483+tmayankv@users.noreply.github.com>
2024-04-27 15:57:41 +0530

    added income

--- Frontend/front/src/components/charts/IncomeChart.jsx
+++ Frontend/front/src/components/charts/IncomeChart.jsx
@@ -1,60 +1,50 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, ColumnSeries, Inject, Tooltip, Legend, Category, DataLabel } from '@syncfusion/ej2-react-charts';
 import { Browser } from '@syncfusion/ej2-base';
-export let data = [
-  { x: 'China', y: 26, tooltipMappingName: 'China' },
-  { x: 'Australia', y: 8, tooltipMappingName: 'Australia' },
-  { x: 'Germany', y: 17, tooltipMappingName: 'Germany' },
-  { x: 'Spain', y: 7, tooltipMappingName: 'Spain' },
-  { x: 'Japan', y: 12, tooltipMappingName: 'Japan' },
-  { x: 'USA', y: 46, tooltipMappingName: 'United States' }
-];
 
-export const barPrimaryXAxis = {
-  valueType: 'Category',
-  interval: 1,
-  majorGridLines: { width: 0 },
-  labelStyle: { color: 'white' },
-  labelIntersectAction: Browser.isDevice ? 'None' : 'Trim',
-  labelRotation: Browser.isDevice ? -45 : 0,
+const IncomeChart = ({ incomeData }) => {
+  const barPrimaryXAxis = {
+    valueType: 'Category',
+    interval: 1,
+    majorGridLines: { width: 0 },
+    labelStyle: { color: 'white' },
+    labelIntersectAction: Browser.isDevice ? 'None' : 'Trim',
+    labelRotation: Browser.isDevice ? -45 : 0,
+  };
 
+  const barPrimaryYAxis = {
+    title: 'Amount',
+    majorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    lineStyle: { width: 0 },
+    labelStyle: { color: '#f0f9ff' },
+    titleStyle: { color: 'white', fontWeight: '100' },
+  };
 
-};
-export const barPrimaryYAxis = {
-  title: 'Medal Count',
-  majorGridLines: { width: 0 },
-  majorTickLines: { width: 0 },
-  lineStyle: { width: 0 },
-  labelStyle: { color: '#f0f9ff' },
-  text
-  
-};
-
-export const barCustomSeries = [
-  {
-    dataSource: data,
-    xName: 'x',
-    yName: 'y',
-    type: 'Column',
-    columnFacet: 'Cylinder',
-    width: 2,
-    columnSpacing: 0.1,
-    fill:'#2563eb',
-    marker: {
-      dataLabel: {
-        visible: true,
-        position: 'Top',
-        font: { fontWeight: '400', color: 'white' },
+  const barCustomSeries = [
+    {
+      dataSource: incomeData,
+      xName: 'source',
+      yName: 'amount',
+      type: 'Column',
+      columnFacet: 'Cylinder',
+      width: 2,
+      columnSpacing: 0.1,
+      fill: '#2563eb',
+      marker: {
+        dataLabel: {
+          visible: true,
+          position: 'Top',
+          font: { fontWeight: '400', color: 'white' },
+        },
       },
     },
-  },
-];
+  ];
 
-const IncomeChart = () => {
   return (
     <div className="flex items-center justify-center p-2 w-full rounded-lg text-white" style={{ background: 'linear-gradient(to top, rgb(2, 0, 94, 0.2), rgba(0, 0, 0, 0.4))', backdropFilter: 'blur(10px)' }}>
       <ChartComponent
-      className="min-[390px]:w-[85%] max-[1200px]:w-[88%] max-[225px]:w-[65%] "
+        className="min-[390px]:w-[85%] max-[1200px]:w-[88%] max-[225px]:w-[65%] "
         id="charts"
         primaryXAxis={barPrimaryXAxis}
         primaryYAxis={barPrimaryYAxis}
@@ -62,7 +52,6 @@ const IncomeChart = () => {
         tooltip={{ enable: true }}
         background={''}
         legendSettings={{ background: '#2563eb' }}
-        
       >
         <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
         <SeriesCollectionDirective>
