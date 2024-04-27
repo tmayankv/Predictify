4365123f438d2bc76b2e9e88c93609f95d20feb7
tmayankv <106771483+tmayankv@users.noreply.github.com>
2024-04-27 21:40:47 +0530

    added

--- Backend/Application/Api/income.py
+++ Backend/Application/Api/income.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 import hashlib
 import json
+from flask_cors import CORS
 
 
 from Application.models import *
@@ -13,6 +14,7 @@ from Application.exception import *
 from app import app, api, db
 
 
+CORS(app,orgins=['https://localhost:5173'])
 
 @app.route("/api/income/<string:username>", methods=["GET"])
 @app.route("/api/income/<int:id>", methods=["GET"])
