fae3001e3e97f0e2cf51091ddbe8d1a1d6f84453
tmayankv <106771483+tmayankv@users.noreply.github.com>
2024-04-28 12:08:08 +0530

    added chart

--- Frontend/front/src/components/IncomeTable.jsx
+++ Frontend/front/src/components/IncomeTable.jsx
@@ -1,45 +1,37 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Edit, Page, Selection, Sort, Filter, Toolbar, Inject } from '@syncfusion/ej2-react-grids';
 
-const customerGridStatus = (props) => (
+const customerGridRecurring = (props) => (
   <div className="flex gap-2 justify-center items-center text-gray-700 capitalize">
-    <p style={{ background: props.StatusBg }} className="rounded-full h-3 w-3" />
-    <p>{props.Status}</p>
+    <p style={{ background: props.Recurring ==="True" ? '#8BE78B': '#b91c1c' }} className="rounded-full h-3 w-3" />
+    <p>{props.Recurring}</p>
   </div>
 );
 
 export const customersGrid = [
   { type: 'checkbox', width: '50' },
-  { field: 'ProjectName', headerText: 'Project Name', width: '150', textAlign: 'Center' },
-  { field: 'Status', headerText: 'Status', width: '130', format: 'yMd', textAlign: 'Center', template: customerGridStatus },
-  { field: 'Weeks', headerText: 'Weeks', width: '100', format: 'C2', textAlign: 'Center' },
-  { field: 'Budget', headerText: 'Budget', width: '100', format: 'yMd', textAlign: 'Center' },
-  { field: 'Location', headerText: 'Location', width: '150', textAlign: 'Center' },
-  { field: 'CustomerID', headerText: 'Customer ID', width: '120', textAlign: 'Center', isPrimaryKey: true },
+  { field: 'Source', headerText: 'Source', width: '150', textAlign: 'Center' },
+  { field: 'Recurring', headerText: 'Recurring', width: '130', format: 'yMd', textAlign: 'Center', template: customerGridRecurring },
+  { field: 'Amount', headerText: 'Amount', width: '100', format: 'yMd', textAlign: 'Center' },
+  { field: 'Date', headerText: 'Date', width: '150', textAlign: 'Center' },
+  { field: 'ActionID', headerText: 'ActionID', width: '120', textAlign: 'Center', isPrimaryKey: true },
 ];
 
 export const customersData = [
   {
-    CustomerID: 1001,
-    CustomerName: 'Nirav Joshi',
-    CustomerEmail: 'nirav@gmail.com',
-    ProjectName: 'Hosting Press HTML',
-    Status: 'Active',
-    StatusBg: '#8BE78B',
-    Weeks: '40',
-    Budget: '$2.4k',
-    Location: 'India',
+    ActionID: 1001,
+    Source: 'Nirav Joshi',
+    Recurring: 'False',
+    Date: new Date().toDateString(),
+    Amount: 'Rs 2.4k',
   },
   {
-    CustomerID: 1002,
-    CustomerName: 'Sunil Joshi',
+    ActionID: 1002,
+    Source: 'Sunil Joshi',
     CustomerEmail: 'sunil@gmail.com',
-    ProjectName: 'Elite Admin',
-    Status: 'Active',
-    StatusBg: '#8BE78B',
-    Weeks: '11',
-    Budget: '$3.9k',
-    Location: 'India',
+    Recurring: 'True',
+    Date: new Date().toDateString(),
+    Amount: 'Rs 3.9k',
   },
 ];
 
@@ -51,8 +43,8 @@ function IncomeTable() {
   };
 
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <h1 className="text-3xl font-semibold text-black italic text-center mb-2">
+    <div className="m-2 md:m-10 p-2 md:p-10 rounded-3xl"  style={{ background: 'linear-gradient(to right, rgb(0,185,222,0.3), rgba(0, 0, 0, 0.8))', backdropFilter: 'blur(10px)' }}>
+      <h1 className="text-3xl font-semibold text-white italic text-center mb-2">
         INCOME DATA:
       </h1>
       <GridComponent
